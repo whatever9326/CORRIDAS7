56a90d9b3c50164525a3265bc9c2f10cf70fbdfb
whatever9326 <whatever9326@gmail.com>
2025-06-15 01:04:03 -0600

    Update GeneradorGS.jsx

--- src/GeneradorGS.jsx
+++ src/GeneradorGS.jsx
@@ -35,7 +35,7 @@ export default function GeneradorGS() {
   };
 
   const generarSKUs = () => {
-    const tallas = [23, 24, 25, 26, 27, 28, 29, 30];
+    const tallas = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
     const resumen = {};
     const noEncontrados = [];
 
